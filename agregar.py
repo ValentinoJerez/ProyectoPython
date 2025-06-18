fd1a254537aa3b66b90ab3b150a6c23d5e4ef804
ValentinoJerez <valenjerez69@gmail.com>
2025-06-18 18:58:15 -0300

    agregar

---
+++ agregar.py
@@ -0,0 +1,70 @@
+import sqlite3
+
+DB_NAME = "inventario.db" #Defino la base de datos nuevamente
+
+def agregar_producto():
+    print("Agregar un nuevo producto")
+    
+    #Nombre
+    while True:
+        nombre = input("Ingrese el nombre del producto: ")
+        if nombre == "":
+            print("El nombre del producto no puede estar vacío")
+        else:
+            break
+
+    #Descripcion
+    while True:
+        descripcion = input("Ingrese la descripción del producto: ") #Descripcion puede estar vacio
+        break
+
+    #Cantidad
+    while True:
+        cantidad = input("Ingrese la cantidad del producto: ")
+        if cantidad == "":
+            print("La cantidad del producto no puede estar vacía")
+        else:
+            break
+
+    #Precio
+    while True:
+        precio = int(input("Ingrese el precio del producto: ")) #Pido un entero
+        if precio == "":
+            print("El precio del producto no puede estar vacío")
+        elif precio > 0: #Verifico que el precio sea mayor a 0
+            break
+        else:
+            print("El precio debe ser mayor a 0")
+
+    #Categoria
+    while True:
+        categoria = input("Ingrese la categoria del producto: ")
+        if categoria == "":
+            print("La categoria del producto no puede estar vacía")
+        else:
+            break
+
+    #Conexion con Base de Datos
+    try:
+        conexion = sqlite3.connect(DB_NAME)
+        cursor = conexion.cursor()
+        
+        sql_insert = """
+            INSERT INTO productos (nombre, descripcion, cantidad, precio, categoria)
+            VALUES (?, ?, ?, ?, ?);
+            """
+        
+        valores = (nombre, descripcion, cantidad, precio, categoria)
+
+        cursor.execute(sql_insert, valores)
+        conexion.commit() #Guardo los cambios
+
+        print(f"Producto {nombre} agregado con exito ")
+
+    except sqlite3.Error as e: #Manejo de errores
+        print(f"Error al agregar producto: {e}")
+        if conexion:
+            conexion.rollback() #Desago los cambios en caso de error
+
+    finally:
+        conexion.close() #Cierro conexion
