5fa94859aaf2352d8ec0cc8ff495e63613e695d2
ValentinoJerez <valenjerez69@gmail.com>
2025-06-18 18:42:00 -0300

    eliminar

---
+++ eliminar.py
@@ -0,0 +1,44 @@
+import sqlite3
+import mostrar
+
+DB_NAME = "inventario.db"
+
+def eliminar_productos():
+    print("Eliminar Producto")
+    #Muestro los productos disponibles
+    mostrar.mostrar_productos()
+
+    while True:
+        print("¿Que producto desea eliminar?")
+        producto = int(input("Ingrese el ID del producto que desea eliminar (o 0 para cancelar): "))
+        if producto == 0:
+            print("Eliminacion de producto cancelada")
+            return
+        elif producto < 0:
+            print("¡Error! El ID no puede ser un número negativo. Intente de nuevo.")
+        else:
+            break
+    
+    #Conexion DB
+    try:
+        conexion = sqlite3.connect(DB_NAME)
+        cursor = conexion.cursor
+        
+        sql_delete = "DELETE FROM productos WHERE id = ?;"
+        
+        #Paso ID como tupla
+        cursor.execute(sql_delete, (producto,))
+        conexion.commit()
+        
+        #Verifico si se elimino
+        if cursor.rowcount > 0:
+            print(f"Producto con ID {producto} eliminado exitosamente.")
+        else:
+            print(f"No se encontró un producto con ID {producto}. No se eliminó nada.")
+    
+    except sqlite3.Error as e:
+        print(f"¡Error! No se pudo eliminar el producto {e}")
+        if conexion:
+            conexion.rollback() #Borro los cambios en caso de error
+    finally:
+        conexion.close()
